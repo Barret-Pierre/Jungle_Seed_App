51a9bddad9310334c4c7e039eb69fd4b965a8147
Barret-Pierre <pierrealexandrebarret.pro@gmail.com>
2022-07-08 15:17:30 +0200

    Add useEffect

--- src/components/App.js
+++ src/components/App.js
@@ -5,17 +5,23 @@ import ShoppingList from "./ShoppingList";
 import Footer from "./Footer";
 import "../styles/layout.css";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
-  const [cart, updateCart] = useState([])
+  const savedCart = localStorage.getItem('cart')
+  const [cart, updateCart] = useState(savedCart ? JSON.parse(savedCart) : [])
+  useEffect(() => {
+    localStorage.setItem('cart', JSON.stringify(cart))
+  }, [cart]);
+
+
   return (
     <div>
       <Banner />
       <Description />
       <div className="js-layout-inner">
-        <Cart cart={cart} updateCart={updateCart}/>
-        <ShoppingList  cart={cart} updateCart={updateCart}/>
+        <Cart cart={cart} updateCart={updateCart} />
+        <ShoppingList cart={cart} updateCart={updateCart} />
       </div>
       <Footer />
     </div>
--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,30 +1,41 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "../styles/Cart.css"
 
-function Cart({cart, updateCart}) {
-    
-    const [isOpen, setIsOpen] = useState(false)
-    const total = cart.reduce(
-        (curr, plant) => curr + plant.amount*plant.price, 0)
+function Cart({ cart, updateCart }) {
+
+    const [isOpen, setIsOpen] = useState(true)
+    const items = Object.keys(cart)
+    const total = items.reduce(
+        (curr, item) => curr + cart[item].amount * cart[item].price, 0)
+
+    useEffect(() => {
+        document.title = `JS: ${total}€ d'achat`
+    }, [total])
 
     return isOpen ? (
         <div className="js-cart">
             <button onClick={() => setIsOpen(false)} className="js-cart-toggle-button">Fermer</button>
-            <h2>Panier</h2>
-            {cart.map((plant) =>
-                (
-                    <div key={plant.id}> 
-                        {plant.name} {plant.price}€ X {plant.amount}
-                    </div>
-                ))}
-            <h3>Total: {total}€</h3>
-            <button onClick={() => updateCart([])} className="js-cart-add-button">Vider le pannier</button>
+            {cart.length > 0 ? (
+                <div>
+                    <h2>Panier</h2>
+                    {cart.map((plant) =>
+                    (
+                        <div key={plant.id}>
+                            {plant.name} {plant.price}€ X {plant.amount}
+                        </div>
+                    ))}
+                    <h3>Total: {total}€</h3>
+                    <button onClick={() => updateCart([])} className="js-cart-add-button">Vider le pannier</button>
+                </div>
+            ) : (
+                <div>Votre Pannier est vide</div>
+            )}
         </div>
     ) : (
         <div className="js-cart-closed">
             <button onClick={() => setIsOpen(true)} className="js-cart-toggle-button">Ouvrir le pannier</button>
         </div>
-        
+
     )
 
 }
